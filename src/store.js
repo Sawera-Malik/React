dec54eeb08acec0ea1cf6cc51c445a915ee13e6d
Sawera Malik <attachannar8@gmail.com>
2024-11-05 12:13:43 +0500

    Initial commit

---
+++ src/store.js
@@ -0,0 +1,13 @@
+
+import { configureStore } from '@reduxjs/toolkit';
+import tasksReducer from './Reducer/sliceauth';
+
+
+const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    
+  },
+});
+
+export default store;
